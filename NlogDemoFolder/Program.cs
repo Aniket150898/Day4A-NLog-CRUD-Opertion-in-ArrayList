276e9b5965d830fdad51634140e24ee66c4655ea
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-19 21:24:03 +0530

    [Aniket]- Print Welcome message

---
+++ NlogDemoFolder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NlogDemoFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Everyone!!!Performs NLog & CRUD operation in ArrayList & Dictionary");
+        }
+    }
+}
