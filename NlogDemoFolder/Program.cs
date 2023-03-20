fc1d960e624f9403d2e9112c7fa8137eafaab26c
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-20 18:09:24 +0530

    [Aniket]- Perform Nlog

--- NlogDemoFolder/Program.cs
+++ NlogDemoFolder/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Everyone!!!Performs NLog & CRUD operation in ArrayList & Dictionary");
+            AddNumber obj = new AddNumber();
+            obj.Sum(2, 3);
+            Console.WriteLine("This is a Logger Demo");
+            Console.ReadKey();
         }
     }
 }
