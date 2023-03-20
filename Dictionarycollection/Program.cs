d5570f77ed7dbf3a144fc9ec097c123f6d4a2e01
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-20 22:03:00 +0530

    [Aniket]- Perform CRUD operation in Dictionary Collection

---
+++ Dictionarycollection/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Dictionarycollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perfoming CRUD operation in Dicitionary Collection!");
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>(); //Create a new item in Dictionary
+            myDictionary.Add("apple", 3);
+            myDictionary.Add("banana", 5);
+
+            myDictionary["apple"] = 4; //Update value in a Dictionary of key Apple
+
+            myDictionary.Remove("banana"); //Delete value of key from a Dictionary
+
+            foreach (var value in myDictionary) //Reading An Dictionarycollection values
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
